72147dabde5f089a7a0ce61f402758ff4826598b
Tracy Rotton <tracy@taupecat.com>
2014-11-05 23:04:56 -0500

    i18n

--- tc-disable-browser-upgrade-warning.php
+++ tc-disable-browser-upgrade-warning.php
@@ -3,16 +3,21 @@
 Plugin Name: TC Disable Browser Upgrade Warning
 Plugin URI: http://www.taupecat.com/wordpress-plugins/tc-disable-browser-upgrade-warning/
 Description: Disable the warning you see on the dashboard if you're using an older version of a particular browser.
-Version: 1.0.1
+Version: 1.1
 Author: Tracy Rotton
 Author URI: http://www.taupecat.com/
-License: GPLv2
+License: MIT
 */
 
 function tc_disable_browser_upgrade_warning() {
     remove_meta_box( 'dashboard_browser_nag', 'dashboard', 'normal' );
 }
-
 add_action( 'wp_dashboard_setup', 'tc_disable_browser_upgrade_warning' );
 
-?>
+/**
+ * I18n
+ */
+function tc_dbuw_i18n() {
+	load_plugin_textdomain('tc-dbuw', false, plugin_dir_path( __FILE__ ) . 'languages/' );
+}
+add_action( 'init', 'tc_dbuw_i18n' );
